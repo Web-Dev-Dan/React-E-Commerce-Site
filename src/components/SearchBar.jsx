bf6583b33c7f7ff2bdae583430941856e724bf5d
Dan <webdevd4n@gmail.com>
2022-05-17 00:00:12 +0900

    Added Search Button functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,8 +5,13 @@ export default function SearchBar() {
     const [inputText, setInputText] = useState('');
 
     const handleInputChange = (e) => {
-        console.log(e.target.value);
+        // console.log(e.target.value);
         setInputText(e.target.value);
+    }
+
+    const handleSeachClick = () => {
+        console.log(inputText);
+        setInputText(''); // Not working...
     }
 
     return (
@@ -23,7 +28,7 @@ export default function SearchBar() {
                 type="text"
                 placeholder="Search..."
             />
-            <button className="search-bar__button btn-primary">
+            <button onClick={handleSeachClick} className="search-bar__button btn-primary">
                 <i className="search-bar__button--icon fa-solid fa-magnifying-glass"></i>
             </button>
         </div>
