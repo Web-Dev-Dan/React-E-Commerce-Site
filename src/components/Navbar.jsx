363e4dfa369cb17ef2580e75a26a22f10b56a61d
Dan <webdevd4n@gmail.com>
2022-05-16 23:53:07 +0900

    Repositioned SearchBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,8 +9,10 @@ export default function Navbar() {
     return (
         <nav className="nav">
             <img className="nav__logo" src="../imgs/logo.png"></img>
-            <SearchBar />
-            <UserOptions />
+            <div className="nav__options">
+                <SearchBar />
+                <UserOptions />
+            </div>
         </nav>
     )
 }
