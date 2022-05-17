ef84daa6a840f9081f864107dbd726167ef1ec7b
Dan <webdevd4n@gmail.com>
2022-05-18 00:08:07 +0900

    Added Hero image and text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import './Hero.css';
+import HeroImage from '../imgs/hero-img.png';
 
 export default function Hero() {
 
@@ -12,7 +13,14 @@ export default function Hero() {
 
     return (
         <div className="hero">
-
+            <div className="hero-div hero-div__text">
+                <h2 className="hero-div__text--header">Shopping made easy</h2>
+                <p className="hero-div__text--description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatibus magnam a eius porro. Repellendus beatae delectus dolorem numquam magni!</p>
+                <button className="hero-div__text__button btn-primary">Start Shopping</button>
+            </div>
+            <div className="hero-div">
+                <img className="hero__image" src={HeroImage} alt="A very happy shopper!" />
+            </div>
         </div>
     )
 }
