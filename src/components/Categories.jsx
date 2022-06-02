a8414c43ba1e55185a024a01694f4059c757655f
Dan <webdevd4n@gmail.com>
2022-06-02 20:17:56 +0900

    Added category images

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,10 +1,35 @@
+import CategoriesBox from './CategoriesBox';
+import ClothingPic from '../imgs/clothing-pic.jpg';
+import JewelryPic from '../imgs/jewelry-pic.jpg';
+import AccessoriesPic from '../imgs/accessories-pic.jpg';
+
 
 import './Categories.css';
 
 export default function Categories() {
+    const categoryList = [
+        {
+            'id': 1,
+            'title': 'Clothing',
+            'img': { ClothingPic }
+        },
+        {
+            'id': 2,
+            'title': 'Jewelry',
+            'img': { JewelryPic }
+        },
+        {
+            'id': 3,
+            'title': 'Accessories',
+            'img': { AccessoriesPic }
+        }
+    ];
+
     return (
         <div className="categories">
-            categories
-        </div>
+            {categoryList.map(category => (
+                <CategoriesBox key={category.id} title={category.title} img={category.img} />
+            ))}
+        </div >
     )
 }
---
+++ src/components/CategoriesBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CategoriesBox = ({ key, title, img }) => {
+    return (
+        <div className="categories-box">
+            <h1>{title}</h1>
+        </div>
+    )
+}
+
+export default CategoriesBox;
