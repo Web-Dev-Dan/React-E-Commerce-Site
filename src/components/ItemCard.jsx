8345552fb5262139088e999bde0154e6ceb2090a
Dan <webdevd4n@gmail.com>
2022-06-29 15:25:08 +0900

    Added 'More Info' button to ItemCard

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -11,9 +11,13 @@ export default function ItemCard({ imgSource, title, price }) {
             {/* Price & Buy Button */}
             <div className="item-card__details">
                 <p className="details__price">${price}</p>
-                <button onClick={() => console.log('Item button clicked')} className="details__button btn-primary">
+                <button onClick={() => console.log('Add to basket clicked')} className="details__button btn-primary">
                     <i className="details__button--icon fa-solid fa-cart-plus"></i>
                 </button>
+            </div>
+            <div className="item-card__more-info">
+                <i className="more-info__icon fa-solid fa-circle-info"></i>
+                <a onClick={() => console.log('More info clicked')} className="more-info__text btn-text-link">More Details</a>
             </div>
         </div>
     )
